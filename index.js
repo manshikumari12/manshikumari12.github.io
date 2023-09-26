c5af1504ce24cca8bd3828a09b64377f8765a2b4
Manshi Kumari <119393324+manshikumari12@users.noreply.github.com>
2023-09-26 20:03:03 +0530

    done

--- index.js
+++ index.js
@@ -5,10 +5,7 @@
 
 
 
-
-
-
-// ================================================
+// ==================================
 $(document).ready(function () {
   $(window).scroll(function () {
 
@@ -39,6 +36,9 @@ $(document).ready(function () {
   $("#nav-menu .menu li a").click(function () {
     // applying again smooth scroll on menu items click
     $("html").css("scrollBehavior", "smooth");
+
+    $("#nav-menu .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
   });
 
 
@@ -67,27 +67,27 @@ $(document).ready(function () {
 
 
   // owl carousel script
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeOut: 1000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
-  });
+  // $(".carousel").owlCarousel({
+  //   margin: 20,
+  //   loop: true,
+  //   autoplay: true,
+  //   autoplayTimeOut: 1000,
+  //   autoplayHoverPause: true,
+  //   responsive: {
+  //     0: {
+  //       items: 1,
+  //       nav: false,
+  //     },
+  //     600: {
+  //       items: 2,
+  //       nav: false,
+  //     },
+  //     1000: {
+  //       items: 3,
+  //       nav: false,
+  //     },
+  //   },
+  // });
 });
 
 
@@ -110,6 +110,5 @@ function openDrive2() {
   window.open(
     (href =
       "https://drive.google.com/file/d/1R9jzsCSUfCeiMgMpjzJcyt3wTpJW2Cmh/view?usp=sharing")
-      
   );
 }
